9e43dab44d8d3ce581183c4ed87983333c83ab92
seungh0 <will.seungho@gmail.com>
2020-09-26 19:51:26 +0900

    feat: run.sh add ecr login

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,8 @@ DOCKER_APP_NAME=month-app
 
 cd $DIRECTORY
 
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 751264546625.dkr.ecr.ap-northeast-2.amazonaws.com
+
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
