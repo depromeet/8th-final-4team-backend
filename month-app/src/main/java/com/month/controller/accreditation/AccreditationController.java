57cca116b275766640833b03465c742cfed109f3
LeeYuRi1 <dbf51561590@gmail.com>
2020-11-02 15:51:00 +0900

    feat: challenge 인증을 저장하는 기능 추가

---
+++ month-app/src/main/java/com/month/controller/accreditation/AccreditationController.java
@@ -0,0 +1,30 @@
+package com.month.controller.accreditation;
+
+import com.month.config.resolver.LoginMember;
+import com.month.controller.ApiResponse;
+import com.month.service.accreditation.AccreditationService;
+import com.month.service.accreditation.dto.response.AccreditationRequest;
+import com.month.type.session.MemberSession;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class AccreditationController {
+
+    private final AccreditationService accreditationService;
+
+    @ApiOperation("챌린지 인증을 저장하는 API")
+    @PostMapping("/api/v1/accreditation")
+    public ApiResponse<String> saveAccreditation(@LoginMember MemberSession memberSession,
+                                                 AccreditationRequest request,
+                                                 @RequestPart(value = "image", required = false) MultipartFile image) {
+        if (image != null) request.setImage(image);
+        accreditationService.saveAccreditation(memberSession.getMemberId(), request);
+        return ApiResponse.of("챌린지 인증 완료");
+    }
+}
---
+++ month-domain/src/main/java/com/month/domain/accreditation/repository/AccreditationRepository.java
@@ -0,0 +1,8 @@
+package com.month.domain.accreditation.repository;
+
+import com.month.domain.accreditation.Accreditation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccreditationRepository extends JpaRepository<Accreditation, Long> {
+
+}
