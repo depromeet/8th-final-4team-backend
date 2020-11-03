10e1b5e01c11ab3ce217eb1fcacc4115a988537f
seungh0 <will.seungho@gmail.com>
2020-11-03 17:11:41 +0900

    feat: 소셜 로그인 통합 전까지 테스트를 위한 테스트 API 추가.
    - 기존의 /test-auth 는 무조건 같은 사용자임을 보장하도록 변경
    - 새로운 /test-auth/custom 을 통해 이메일이 다른 계정을 생성할 수 있음.

--- month-app/src/main/java/com/month/controller/MainController.java
+++ month-app/src/main/java/com/month/controller/MainController.java
@@ -6,6 +6,7 @@ import com.month.domain.member.MemberRepository;
 import com.month.type.session.MemberSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -27,7 +28,22 @@ public class MainController {
 	// TODO Prod 배포시 지우기
 	@GetMapping("/test-auth")
 	public String testAuth() {
-		Member member = memberRepository.save(MemberCreator.create("test@test.com", "name", null, "uid"));
+		String email = "test@test.com";
+		Member member = memberRepository.findMemberByEmail(email);
+		if (member == null) {
+			member = memberRepository.save(MemberCreator.create(email, "name", null, "uid"));
+		}
+		httpSession.setAttribute(LOGIN_SESSION, MemberSession.of(member.getId()));
+		return httpSession.getId();
+	}
+
+	// TODO 소셜 로그인 Integration 하고 지우기
+	@GetMapping("/test-auth/custom")
+	public String testAuth(@RequestParam String email) {
+		if (memberRepository.findMemberByEmail(email) != null) {
+			throw new IllegalArgumentException("이미 존재하는 멤버");
+		}
+		Member member = memberRepository.save(MemberCreator.create(email, "테스트 계정", null, email + "uid"));
 		httpSession.setAttribute(LOGIN_SESSION, MemberSession.of(member.getId()));
 		return httpSession.getId();
 	}
